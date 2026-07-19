3ba17d23c86604cb702133701394f04195f8d605
EddyGiusepe <eddychirinos.unac@gmail.com>
2026-07-18 22:40:54 -0300

    feat: add reusable Toaster component for displaying customizable toast notifications

---
+++ 1_describing_a_cars_license_plate/frontend_embedl/src/components/ui/sonner.tsx
@@ -0,0 +1,31 @@
+/**
+ * Senior Data Scientist.: Dr. Eddy Giusepe Chirinos Isidro
+ *
+ * sonner.tsx
+ * ==========
+ * Reusable Sonner component used to display toast notifications.
+ * Generic toast notifications!
+ */
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  const { theme = "system" } = useTheme();
+
+  return (
+    <Sonner
+      theme={theme as ToasterProps["theme"]}
+      className="toaster group"
+      style={
+        {
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
